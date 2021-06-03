ce177c5c2b48d6784bbea8fefa95d77a3ddf8b62
alpha-shri <shrijeetkumarpanda@gmail.com>
2021-06-03 17:57:26 +0530

    check

--- src/main/java/com/azure/SpringAzureAppApplication.java
+++ src/main/java/com/azure/SpringAzureAppApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureAppApplication {
 
 	@GetMapping("/home")
 	public String homePage(){
-		return "Congratts.. Application deployed successfully in MS Azure..";
+		return "Congratulations.. Application deployed successfully in MS Azure..";
 	}
 
 	public static void main(String[] args) {
